4539f78def5cb2c5c48333a45799d9ab24095953
nischaydesk <vimalkumartelwa1985@gmail.com>
2026-09-08 20:26:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -353,7 +353,8 @@ async function sendQuestionToGroq() {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "llama-3.3-70b-versatile",
+       model: "llama-3.1-8b-instant",
+
         messages: [
           {
             role: "system",
